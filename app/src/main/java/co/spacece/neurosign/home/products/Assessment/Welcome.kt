8673f2e8a20e3188aa6f37d9b63cc6597b3304ba
Masoom08 <masoomsingh0801@gmail.com>
2025-03-31 13:02:00 +0530

    version 1.8 EarlyAssessment Page 4 ui problem

--- app/src/main/java/co/spacece/neurosign/home/products/Assessment/Welcome.kt
+++ app/src/main/java/co/spacece/neurosign/home/products/Assessment/Welcome.kt
@@ -25,7 +25,7 @@ import co.spacece.neurosign.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-
+// Problem in UI
 @Composable
 fun WelcomeAssessment(navController: NavController) {
     val appPrimary = Color(0xFFF8A800)
@@ -39,95 +39,119 @@ fun WelcomeAssessment(navController: NavController) {
         Card(
             modifier = Modifier
                 .padding(16.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth().background(color= Color.White),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
             shape = RoundedCornerShape(16.dp)
         ) {
-            Column(
-                modifier = Modifier.padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp), // Keep height as per your design
+                contentAlignment = Alignment.TopEnd
             ) {
-                // Back button and Welcome text
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(
-                        onClick = { /* Back action */ },
-                        modifier = Modifier
-                            .background(Color.White, RoundedCornerShape(20.dp))
-                            .border(1.dp, appPrimary, RoundedCornerShape(20.dp))
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "Back",
-                            tint = appPrimary
-                        )
-                    }
-                    Text(
-                        text = "Welcome!",
-                        fontSize = 32.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = appPrimary,
-                        modifier = Modifier.padding(start = 16.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(32.dp))
-
-                Text(
-                    text = "Discover Your Unique Learning Journey",
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
+                Image(
+                    painter = painterResource(id = R.drawable.top),
+                    contentDescription = "Top Design",
+                    modifier = Modifier
+                        .width(150.dp) // Adjust width to your requirement
+                        .height(150.dp), // Ensure it maintains the required size
+                    contentScale = ContentScale.FillWidth
                 )
 
-                Spacer(modifier = Modifier.height(16.dp))
+            }
+            // Back button and Welcome text
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
 
+                IconButton(
+                    onClick = { navController.popBackStack()  },
+                    modifier = Modifier
+                        .background(Color.White, RoundedCornerShape(20.dp))
+                        .border(1.dp, appPrimary, RoundedCornerShape(20.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back",
+                        tint = appPrimary
+                    )
+                }
                 Text(
-                    text = "Select an activity",
+                    text = "Welcome!",
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Medium,
                     color = appPrimary,
-                    fontSize = 12.sp
+                    modifier = Modifier.padding(start = 16.dp)
                 )
 
-                Spacer(modifier = Modifier.height(16.dp))
 
-                // Activity Cards Grid
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    ActivityCard(
-                        title = "Start Assessment",
-                        iconRes = R.drawable.start,
-                        isActive = false
-                    )
-                    ActivityCard(
-                        title = "Pattern Recognition",
-                        iconRes = R.drawable.pattern,
-                        isActive = true
-                    )
+            }
 
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    ActivityCard(
-                        title = "Reading Assessment",
-                        iconRes = R.drawable.read,
-                        isActive = false
-                    )
-                }
+            Spacer(modifier = Modifier.height(32.dp))
 
-                Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Discover Your Unique Learning Journey",
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
 
-                Button(
-                    onClick = { /* Generate Assessment action */ },
-                    colors = ButtonDefaults.buttonColors(containerColor = appPrimary)
-                ) {
-                    Text("Generate Assessment")
-                }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Select an activity",
+                color = appPrimary,
+                fontSize = 12.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Activity Cards Grid
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ActivityCard(
+                    title = "Start Assessment",
+                    iconRes = R.drawable.start,
+                    isActive = false
+                )
+                ActivityCard(
+                    title = "Pattern Recognition",
+                    iconRes = R.drawable.pattern,
+                    isActive = true
+                )
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ActivityCard(
+                    title = "Reading Assessment",
+                    iconRes = R.drawable.read,
+                    isActive = false
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = { /* Generate Assessment action */ },
+                colors = ButtonDefaults.buttonColors(containerColor = appPrimary)
+            ) {
+                Text("Generate Assessment")
             }
         }
+        // Bottom Image
+        Image(
+            painter = painterResource(id = R.drawable.bottom),
+            contentDescription = "Bottom Design",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            contentScale = ContentScale.FillWidth
+        )
     }
 }
 @Composable
